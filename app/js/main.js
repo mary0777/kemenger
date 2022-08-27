30f243d3e8ff2f437104cb7720e16a7d75bac0b2
mary0777 <maksymenko.m.07@gmail.com>
2022-08-27 22:07:00 +0300

    added section about + fixed header + scroll by section

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
- 
+
   $('.hero__popup').magnificPopup({
     type: 'inline',
     preloader: false,
     focus: '#name',
-  
+
     callbacks: {
       beforeOpen: function () {
         if ($(window).width() < 700) {
@@ -13,10 +13,32 @@ $(function () {
           this.st.focus = '#name';
         }
       },
-       
     }
   });
- 
-  
-   
+
+
+  $('.menu__link').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top - 150;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+
+
+
+});
+
+
+
+$(window).scroll(function () {
+
+  var scroll = $(window).scrollTop();
+  if (scroll >= 700) {
+    $('.header').addClass('fixed');
+  } else {
+    $('.header').removeClass('fixed');
+  }
 });
